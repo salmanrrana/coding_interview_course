3424088d92441277936834c5a5443851384d757b
Salman Rana <salman.r.rana@gmail.com>
2019-01-20 11:55:46 -0600

    queue from stacks question resolved

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -12,8 +12,48 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.first = new Stack();
+    this.second = new Stack();
+  }
+
+  add(record) {
+    this.first.push(record);
+  }
+
+  // iterate through all records in stack a and push into stack b
+  // using the peek method to see and make sure it has records .
+  remove() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+    // saves a reference to the last thing that got put into stack b
+    const record = this.second.pop();
+
+    //This will return the data from stack b to stack a
+    while (this.second.peek()) {
+      // takes one record at a time and pops it out and into stack a
+      this.first.push(this.second.pop());
+    }
+    // now we are ready to return the record
+    return record;
+  }
+
+  peek() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+    const record = this.second.peek();
+
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+
+    return record;
+  }
+}
 
 module.exports = Queue;
